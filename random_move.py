c3553da4c778e613b1c85336e905670a8462483e
yuchengwang1121 <73687292+yuchengwang1121@users.noreply.github.com>
2021-04-07 12:28:03 +0800

    Create random_move.py

---
+++ random_move.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('robot_teleop')
+import rospy
+import random
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+class Teleop:
+    def __init__(self):
+        pub = rospy.Publisher('cmd_vel', Twist)
+        rospy.init_node('robot_teleop')
+        rate = rospy.Rate(rospy.get_param('~hz', 0.1))
+        self.cmd = None
+
+        cmd = Twist()
+        cmd.linear.x = random.uniform(-0.2, 0.5);
+        cmd.angular.z = random.uniform(-0.5, 0.5);
+
+        self.cmd = cmd
+        while not rospy.is_shutdown():
+            str = "hello world %s" % rospy.get_time()
+            rospy.loginfo(str)
+            pub.publish(self.cmd)
+            rate.sleep()
+
+if __name__ == '__main__':Teleop()
