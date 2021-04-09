2206f451e7a727f2d159c1089aa8574f50cbf79c
yuchengwang1121 <73687292+yuchengwang1121@users.noreply.github.com>
2021-04-09 10:26:32 +0800

    Update interactive_marker.cpp

--- myshape/interactive_marker.cpp
+++ myshape/interactive_marker.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <interactive_markers/interactive_marker_server.h>
 
+//再來是寫回傳的值，我們用INFO印出來看
 void processFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback){
   ROS_INFO_STREAM(feedback->marker_name << " is now at "
       << feedback->pose.position.x << ", " << feedback->pose.position.y
@@ -8,10 +9,12 @@ void processFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr
 
 }
 int main (int argc, char** argv){
+  //初始化節點並架server
   ros::init(argc, argv, "simple_marker");
 
   interactive_markers::InteractiveMarkerServer server("simple_marker");
 
+  //一些基本設置
   visualization_msgs::InteractiveMarker int_marker;
   int_marker.header.frame_id = "/myfirstinteraction";
   int_marker.header.stamp = ros::Time::now();
@@ -23,12 +26,13 @@ int main (int argc, char** argv){
   box_marker.scale.x = 1.0;
   box_marker.scale.y = 1.0;
   
-box_marker.scale.z = 1.0;
+  box_marker.scale.z = 1.0;
   box_marker.color.r = 0.5;
   box_marker.color.g = 0.5;
   box_marker.color.b = 0.5;
   box_marker.color.a = 1.0;
 
+  //把資料丟給int_marker
   visualization_msgs::InteractiveMarkerControl box_control;
   box_control.always_visible = true;
   box_control.markers.push_back( box_marker );
@@ -42,6 +46,7 @@ box_marker.scale.z = 1.0;
 
   int_marker.controls.push_back(rotate_control);
 
+  //把int_marker、Feedback塞給server
   server.insert(int_marker, &processFeedback);
 
   server.applyChanges();
