b3004378a6810b5b3a789b6d564e6acb954080a0
yuchengwang1121 <73687292+yuchengwang1121@users.noreply.github.com>
2021-04-09 10:24:03 +0800

    Update point_and_line.cpp

--- myshape/point_and_line.cpp
+++ myshape/point_and_line.cpp
@@ -12,6 +12,7 @@ int main ( int argc, char** argv){
 
   float f = 0.0;
   while(ros::ok()){
+    //建立三個visualization_msgs/Marker 的訊息然後給予初始值
     visualization_msgs::Marker points, line_strip, line_list;
     points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "point_and_line";
     points.header.stamp = line_strip.header.stamp = line_list.header.stamp = ros::Time::now();
@@ -19,6 +20,8 @@ int main ( int argc, char** argv){
     points.action = line_strip.action = line_list.action = visualization_msgs::Marker::ADD;
     points.pose.orientation.w = line_strip.pose.orientation.w = line_list.pose.orientation.w = 1.0;
 
+    //接著我們給予三個不同的marker三個不同的變數
+    //再來是設置各自的type
     points.id = 0;
     line_strip.id = 1;
     line_list.id = 2;
@@ -26,6 +29,7 @@ int main ( int argc, char** argv){
     line_strip.type = visualization_msgs::Marker::LINE_STRIP;
     line_list.type = visualization_msgs::Marker::LINE_LIST;
 
+    //設置大小與顏色
     points.scale.x = 0.2;
     points.scale.y = 0.2;
 
@@ -41,6 +45,7 @@ int main ( int argc, char** argv){
     line_list.color.r = 1.0;
     line_list.color.a = 1.0;
 
+    //使用迴圈創造圖形並且發布數據
     for(uint32_t i=0; i<100; ++i){
       float y = 5 * sin(f + i /100.0f * 2 * M_PI);
       float z = 5 * cos(f + i /100.0f * 2 * M_PI);
