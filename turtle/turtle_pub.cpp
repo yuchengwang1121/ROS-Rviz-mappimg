bd4f13872f9af03a26eb11146f61b299134c478b
yuchengwang1121 <73687292+yuchengwang1121@users.noreply.github.com>
2021-04-09 10:10:58 +0800

    Update turtle_pub.cpp

--- turtle/turtle_pub.cpp
+++ turtle/turtle_pub.cpp
@@ -4,17 +4,20 @@
 
 int main(int argc, char** argv){
   
-  ros::NodeHandle nh;
-
+  ros::NodeHandle nh;//initialize the node ros::init(argc,argv,“turtle_publlisher_node”);
+  
+  //create a publisher
   ros::Publisher pub=nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel",1000);
-  srand(time(0));
-  ros::Rate rate(2);
+  srand(time(0));//the random number generator
+  ros::Rate rate(2);//loop 2 times every second
 
-  while(ros::ok){
+  while(ros::ok){//while the node is running
+  //create and set the value of each field
     geometry_msgs::Twist msg;
     msg.linear.x=2*double(rand())/double(RAND_MAX);
     msg.angular.z=4*double(rand())/double(RAND_MAX)-2;
-    pub.publish(msg);
+    pub.publish(msg);//publish the message
+    //send a message to rosout
     ROS_INFO_STREAM("Sending random velocity command:"<<"linear="<<msg.linear.x
               <<"angular="<<msg.angular.z);
     rate.sleep();
